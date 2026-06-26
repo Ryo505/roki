c46f04509144a1c5f9647db4951ddb402c990868
Ryo505 <abdrmsfr9@gmail.com>
2026-06-26 04:35:57 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Roki bot Made by Rynnxzn",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 2,
       },
     ],
   },
